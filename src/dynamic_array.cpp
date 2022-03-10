2514dac3c9c0fdc65d9cd1bce9da57df9878db4f
Ramil Safin <RamilSafNab1996@gmail.com>
2022-03-10 21:11:05 +0300

    Add solution

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -12,65 +12,170 @@ namespace assignment {
       throw std::invalid_argument("capacity is not positive");
     }
 
-    // Write your code here ...
+    // устанавливаем емкость массива
+    capacity_ = capacity;
+
+    // выделяем участок памяти под массив
+    data_ = new int[capacity];
+
+    // инициализируем элементы массива нулевым значением
+    std::fill(data_, data_ + capacity, 0);
   }
 
   DynamicArray::~DynamicArray() {
-    // Write your code here ...
+
+    // высвобождаем выделенную память
+    delete[] data_;
+
+    // обнуляем поля
+    size_ = 0;
+    capacity_ = 0;
+    data_ = nullptr;
   }
 
   void DynamicArray::Add(int value) {
-    // Write your code here ...
+
+    // расширяем емкость массива при заполнении
+    if (size_ == capacity_) {
+      Resize(capacity_ + kCapacityGrowthCoefficient);
+    }
+
+    // добавляем элемент в конец массива
+    data_[size_] = value;
+
+    // увеличиваем размер массива
+    size_ += 1;
   }
 
   bool DynamicArray::Insert(int index, int value) {
-    // Write your code here ...
-    return false;
+
+    // index = 0    => вставка в начало массива
+    // index = size => добавление в конец массива
+
+    // проверка на выход за границы позиции вставки
+    if (index < 0 || index > size_) {
+      return false;
+    }
+
+    // расширяем емкость массива при заполнении
+    if (size_ == capacity_) {
+      Resize(capacity_ + kCapacityGrowthCoefficient);
+    }
+
+    // сдвигаем все элементы правее позиции вставки на 1 вправо
+    std::copy(data_ + index, data_ + size_, data_ + index + 1);
+
+    // вставляем элемент по индексу
+    data_[index] = value;
+
+    // увеличиваем размер массива
+    size_ += 1;
+
+    return true;
   }
 
   bool DynamicArray::Set(int index, int new_value) {
-    // Write your code here ...
-    return false;
+
+    // проверка на выход за пределы массива
+    if (index < 0 || index >= size_) {
+      return false;
+    }
+
+    // устанавливаем значение элемента по индексу
+    data_[index] = new_value;
+
+    return true;
   }
 
   std::optional<int> DynamicArray::Remove(int index) {
-    // Write your code here ...
-    return std::nullopt;
+
+    // проверка на выход за пределы массива
+    if (index < 0 || index >= size_) {
+      return std::nullopt;
+    }
+
+    // сохраняем значение удаляемого элемента
+    const int removed_elem = data_[index];
+
+    // сдвигаем все элементы справа от позиции удаляемого элемента на 1 ячейку влево
+    std::copy(data_ + index + 1, data_ + size_, data_ + index);
+
+    // уменьшаем размер массива
+    size_ -= 1;
+
+    // возвращаем значение удаленного элемента
+    return removed_elem;
   }
 
   void DynamicArray::Clear() {
-    // Write your code here ...
+
+    // обнуляем размер массива
+    size_ = 0;
   }
 
   std::optional<int> DynamicArray::Get(int index) const {
-    // Write your code here ...
-    return std::nullopt;
+
+    // проверка на выход за пределы массива
+    if (index < 0 || index >= size_) {
+      return std::nullopt;
+    }
+
+    // возвращаем значение элемента по индексу
+    return data_[index];
   }
 
   std::optional<int> DynamicArray::IndexOf(int value) const {
-    // Write your code here ...
+
+    // линейный поиск элемента в массиве
+    for (int index = 0; index < size_; index++) {
+
+      if (data_[index] == value) {
+        return index;
+      }
+    }
+
     return std::nullopt;
   }
 
   bool DynamicArray::Contains(int value) const {
-    return false;
+    return IndexOf(value).has_value();
   }
 
   bool DynamicArray::IsEmpty() const {
-    return false;
+    return size_ == 0;
   }
 
   int DynamicArray::size() const {
-    return 0;
+    return size_;
   }
 
   int DynamicArray::capacity() const {
-    return 0;
+    return capacity_;
   }
 
   bool DynamicArray::Resize(int new_capacity) {
-    // Write your code here ...
-    return false;
+
+    // "новая" емкость должна быть больше "текущей"
+    if (new_capacity <= capacity_) {
+      return false;
+    }
+
+    // выделяем "новый" блок памяти большего размера
+    auto* new_data = new int[new_capacity];
+
+    // копируем данные из "текущего" блока памяти в "новый"
+    std::copy(data_, data_ + capacity_, new_data);
+
+    // высвобождаем "текущий" блок памяти
+    delete[] data_;
+
+    // сохраняем адрес на "новый" блок памяти
+    data_ = new_data;
+
+    // устанавливаем "новую" емкость массива
+    capacity_ = new_capacity;
+
+    return true;
   }
 
   // ДЛЯ ТЕСТИРОВАНИЯ
--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -9,61 +9,227 @@ namespace assignment {
   }
 
   void LinkedList::Add(int value) {
-    // Write your code here ...
+
+    // создаем "новый" узел
+    auto* node = new Node(value);
+
+    if (front_ == nullptr) {
+      // если список пустой, то инициализируем "начальный" узел
+      front_ = node;
+    } else {
+      // иначе, добавляем "новый" узел после "конечного"
+      back_->next = node;
+    }
+
+    // обновляем "конечный" узел
+    back_ = node;
+
+    // увеличиваем размер списка
+    size_ += 1;
   }
 
   bool LinkedList::Insert(int index, int value) {
-    // Write your code here ...
-    return false;
+
+    // index = 0    => вставка в начало списка
+    // index = size => вставка в конец списка
+
+    // проверка на выход за границы позиции вставки
+    if (index < 0 || index > size_) {
+      return false;
+    }
+
+    auto* node = new Node(value);
+
+    if (front_ == nullptr) {
+      // пустой список: "начальный" и "конечный" узлы одинаковы
+      front_ = node;
+      back_ = node;
+
+    } else {
+      // вставка в "начало" списка
+      if (index == 0) {
+        node->next = front_;
+        front_ = node;
+
+      } else if (index == size_) {
+        // вставка в "конец" списка
+        back_->next = node;
+        back_ = node;
+
+      } else {
+        // вставка "внутрь" списка
+        auto* prev_node = FindNode(index - 1);
+
+        node->next = prev_node->next;
+        prev_node->next = node;
+      }
+    }
+
+    size_ += 1;
+
+    return true;
   }
 
   bool LinkedList::Set(int index, int new_value) {
-    return false;
+
+    auto* node = FindNode(index);
+
+    if (node == nullptr) {
+      return false;
+    }
+
+    node->value = new_value;
+
+    return true;
   }
 
   std::optional<int> LinkedList::Remove(int index) {
-    // Write your code here ...
-    return std::nullopt;
+
+    // проверка на выход за пределы списка
+    if (index < 0 || index >= size_) {
+      return std::nullopt;
+    }
+
+    // удаление из "начала" списка
+    if (index == 0) {
+      auto* remove_node = front_;
+      const int remove_value = remove_node->value;
+
+      // сдвигаем "начало" списка на следующий узел
+      front_ = front_->next;
+
+      // высвобождаем выделенную под удаляемый узел память
+      delete remove_node;
+
+      // уменьшаем размер списка
+      size_ -= 1;
+
+      return remove_value;
+    }
+
+    // удаление из любой другой позиции
+
+    auto* prev_node = FindNode(index - 1);
+
+    // находим удаляемый узел и его значение
+    auto* removed_node = prev_node->next;
+    const int removed_value = removed_node->value;
+
+    // предыдущий узел "указываем" на следующий после удаляемого узла
+    prev_node->next = removed_node->next;
+
+    // высвобождаем выделенную под удаляемый узел память
+    delete removed_node;
+
+    // уменьшаем размер списка
+    size_ -= 1;
+
+    return removed_value;
   }
 
   void LinkedList::Clear() {
-    // Write your code here ...
+
+    // проходимся от "начала" до "конца" списка
+    for (auto* curr_node = front_; curr_node != nullptr; /* ... */) {
+      // сохраняем адрес удаляемого узла
+      auto* remove_node = curr_node;
+
+      // переходим на следующий узел
+      curr_node = curr_node->next;
+
+      // высвобождаем память под предыдущий узел
+      delete remove_node;
+    }
+
+    // обнуляем поля
+    size_ = 0;
+    front_ = nullptr;
+    back_ = nullptr;
   }
 
   std::optional<int> LinkedList::Get(int index) const {
-    // Write your code here ...
-    return std::nullopt;
+
+    // поиск узла по индексу
+    auto* node = FindNode(index);
+
+    if (node == nullptr) {
+      return std::nullopt;
+    }
+
+    return node->value;
   }
 
   std::optional<int> LinkedList::IndexOf(int value) const {
-    // Write your code here ...
+
+    // отслеживаем текущий индекс и проходимся по всем узлам списка
+    int curr_index = 0;
+
+    for (auto* curr_node = front_; curr_node != nullptr; curr_node = curr_node->next) {
+      if (curr_node->value == value) {
+        return curr_index;
+      }
+
+      curr_index += 1;
+    }
+
     return std::nullopt;
   }
 
   bool LinkedList::Contains(int value) const {
-    return false;
+    return IndexOf(value).has_value();
   }
 
   bool LinkedList::IsEmpty() const {
-    return false;
+    return size_ == 0;
   }
 
   int LinkedList::size() const {
-    return 0;
+    return size_;
   }
 
   std::optional<int> LinkedList::front() const {
-    // Write your code here ...
-    return std::nullopt;
+
+    // проверка на наличие узла
+    if (front_ == nullptr) {
+      return std::nullopt;
+    }
+
+    return front_->value;
   }
 
   std::optional<int> LinkedList::back() const {
-    // Write your code here ...
-    return std::nullopt;
+
+    // проверка на наличие узла
+    if (back_ == nullptr) {
+      return std::nullopt;
+    }
+
+    return back_->value;
   }
 
   Node* LinkedList::FindNode(int index) const {
-    // Write your code here ...
+
+    // проверка на выход за границы списка
+    if (index < 0 || index >= size_) {
+      return nullptr;
+    }
+
+    // возвращаем "конец" списка
+    if (index == size_ - 1) {
+      return back_;
+    }
+
+    // отслеживаем текущий индекс и проходимся по всем узлам списка
+    int curr_idx = 0;
+
+    for (auto* node = front_; node != nullptr; node = node->next) {
+      if (curr_idx == index) {
+        return node;
+      }
+
+      curr_idx += 1;
+    }
+
     return nullptr;
   }
 
